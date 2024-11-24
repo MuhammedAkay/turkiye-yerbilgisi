3a40b86d4aa93c6a227ab2e956c05da182b05ace
MuhammedAkay <106436149+MuhammedAkay@users.noreply.github.com>
2024-11-24 17:31:48 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,7 @@
-import { CityInfoClient } from './modules/cityInfoClient';
-import { WeatherClient } from './modules/weatherClient';
+import { CityClient } from './clients/CityClient';
+import { WeatherClient } from './clients/WeatherClient';
 
-class TurkiyeYerBilgisi {
-  public cityInfoClient: CityInfoClient;
-  public weatherClient: WeatherClient | null;
+export { CityClient, WeatherClient };
 
-  constructor(weatherApiKey?: string, weatherApiProvider?: 'openweathermap' | 'weatherapi') {
-    this.cityInfoClient = new CityInfoClient();
-
-    // Hava durumu API anahtarı ve sağlayıcı seçeneği isteğe bağlıdır
-    if (weatherApiKey && weatherApiProvider) {
-      this.weatherClient = new WeatherClient(weatherApiKey, weatherApiProvider);
-    } else {
-      this.weatherClient = null;
-    }
-  }
-
-  /**
-   * Şehir bilgisi sorgulama fonksiyonu
-   * @param cityName Şehir adı
-   */
-  getCityInfo(cityName: string) {
-    return this.cityInfoClient.getCityInfo(cityName);
-  }
-
-  /**
-   * Hava durumu sorgulama fonksiyonu
-   * @param cityName Şehir adı
-   */
-  async getWeather(cityName: string) {
-    if (!this.weatherClient) {
-      throw new Error('WeatherClient tanımlanmadı. Lütfen bir API anahtarı ve sağlayıcı belirleyin.');
-    }
-    return await this.weatherClient.getWeather(cityName);
-  }
-}
-
-export default TurkiyeYerBilgisi;
+// Eğer require ile kullanım sağlanacaksa, aşağıdaki gibi bir export yapılabilir:
+module.exports = { CityClient, WeatherClient };
